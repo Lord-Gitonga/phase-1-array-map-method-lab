cd2957ad78b60af6637ec6a112affff6e6eb1038
lordish <gitonga403@gmail.com>
2023-03-20 18:48:57 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -11,6 +11,15 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
-}
+
+const tutorialsTitleCase = tutorials.map(tutorial => {
+    const words = tutorial.split("");
+    const capitalizedWords = words.map(word =>{
+         return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+    });
+    const titleCase = capitalizedWords.join("");
+    return titleCase;
+    
+});
+
+console.log(tutorialsTitleCase);
